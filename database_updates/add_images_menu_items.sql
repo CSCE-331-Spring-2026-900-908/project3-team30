d4591da6240e06f149fa0f2d94804555c67e731d
nityakhurana8 <nityakhurana8@gmail.com>
2026-04-06 22:52:33 -0500

    update script

--- database_updates/add_images_menu_items.sql
+++ database_updates/add_images_menu_items.sql
@@ -1,5 +1,5 @@
 ALTER TABLE menu_items
-ADD COLUMN image_url TEXT;
+ADD COLUMN IF NOT EXISTS image_url TEXT;
 
 UPDATE menu_items
 SET image_url = 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2023/6/13/boba-milk-tea.jpg.rend.hgtvcom.1280.1280.85.suffix/1686684207354.webp';
