603cd1c044ae9b8376595a39884ca1e29f8249d0
nityakhurana8 <nityakhurana8@gmail.com>
2026-04-06 21:38:57 -0500

    added sql query for later

---
+++ server/src/main/java/com/project3/server/service/MenuService.java
@@ -0,0 +1,5 @@
+// String sqlStatement =
+//             "SELECT name, price, image FROM menu_items " +
+//             "WHERE category NOT IN ('ice', 'sweetness', 'toppings') " +
+//             "ORDER BY name";
+// ResultSet rs = stmt.executeQuery(sqlStatement);
