82ffa9e2713fb9876bdb19fc064ffbf90c38fa45
“Jade <jazahar@tamu.edu>
2026-04-08 21:42:41 -0500

    added javadoc comments to the rest of the files I (Jade) worked on so far

--- server/src/main/java/com/project3/server/model/XReport.java
+++ server/src/main/java/com/project3/server/model/XReport.java
@@ -1,5 +1,9 @@
 package com.project3.server.model;
 
+/**
+ * This is a model class that represents an X report, which includes the necessary fields for the X report
+ * @author Jade Azahar
+ */
 public class XReport {
     private double totalCash;
     private double totalCard;
@@ -10,6 +14,17 @@ public class XReport {
     private int numCancelled;
     private int numVoided;
 
+    /**
+     * Constructor for XReport
+     * @param totalCash
+     * @param totalCard
+     * @param subtotal
+     * @param tax
+     * @param netTotal
+     * @param numSales
+     * @param numCancelled
+     * @param numVoided
+     */
     public XReport(double totalCash, double totalCard, double subtotal, double tax, double netTotal, int numSales, int numCancelled, int numVoided) {
         this.totalCash = totalCash;
         this.totalCard = totalCard;
@@ -21,34 +36,66 @@ public class XReport {
         this.numVoided = numVoided;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return double for cash, card, etc.
+     */
     public double getTotalCash() {
         return totalCash;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return double for card sales
+     */
     public double getTotalCard() {
         return totalCard;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return double for subtotal of cash and card sales
+     */
     public double getSubtotal() {
         return subtotal;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return double for tax amount
+     */
     public double getTax() {
         return tax;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return double for net total (subtotal + tax)
+     */
     public double getNetTotal() {
         return netTotal;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return int for number of sales, cancelled transactions, voided transactions
+     */
     public int getNumSales() {
         return numSales;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return int for number of cancelled transactions
+     */
     public int getNumCancelled() {
         return numCancelled;
     }
 
+    /**
+     * Getters for XReport fields
+     * @return int for number of voided transactions
+     */ 
     public int getNumVoided() {
         return numVoided;
     }
