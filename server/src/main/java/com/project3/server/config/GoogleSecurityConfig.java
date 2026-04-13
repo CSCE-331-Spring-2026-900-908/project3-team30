b5c793fbbbdb8519e4d8a2bd77dafa0ac99f4d8c
k-sanchxz <karlasanchz@tamu.edu>
2026-04-13 10:38:26 -0500

    fixed backend path for oauth

--- server/src/main/java/com/project3/server/config/GoogleSecurityConfig.java
+++ server/src/main/java/com/project3/server/config/GoogleSecurityConfig.java
@@ -42,46 +42,56 @@ public class GoogleSecurityConfig {
 
                             // only these emails should be allowed in as manager
                             if (email != null && email.equalsIgnoreCase("reveille.bubbletea@gmail.com")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
 
                             // karla
                             } else if (email != null && email.equalsIgnoreCase("karlasanchz@tamu.edu")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
                             } else if (email != null && email.equalsIgnoreCase("sanchezkarly14@gmail.com")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
 
                             //Jade    
                             } else if (email != null && email.equalsIgnoreCase("jazahar@tamu.edu")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
                             }
 
                             // Rylee
                             else if (email != null && email.equalsIgnoreCase("rhunt@tamu.edu")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
                             }
 
                             // Nitya
                             else if (email != null && email.equalsIgnoreCase("nityakhurana@tamu.edu")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
                             }
 
                             // Evie
                             else if (email != null && email.equalsIgnoreCase("e_pugliese@tamu.edu")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
                             }
 
                             // Anisha
                             else if (email != null && email.equalsIgnoreCase("anishatx@tamu.edu")) {
-                                response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                // response.sendRedirect("http://localhost:5173/manager?oauth=success");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager?oauth=success");
                             }
                         
                             else {
-                                response.sendRedirect("http://localhost:5173/manager-login?error=unauthorized");
+                                // response.sendRedirect("http://localhost:5173/manager-login?error=unauthorized");
+                                response.sendRedirect("https://project3-team30.vercel.app/manager-login?error=unauthorized");
+                                
                             }
                         })
 
                         .failureHandler((request, response, exception) -> {
-                            response.sendRedirect("http://localhost:5173/manager-login?error=oauth_failed");
+                            response.sendRedirect("https://project3-team30.vercel.app/manager-login?error=oauth_failed");
                         })
                 )
 
